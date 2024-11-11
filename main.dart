b2db324c711fc1d386efbbe708ba4952bcc79dfe
dhafa dimas hendrawan <samidpplg@gmail.com>
2024-11-11 10:51:32 +0700

    Container Widget ver 0.0

--- main.dart
+++ main.dart
@@ -3,14 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-    this.onPressed,
-  });
-
-  final VoidCallback? onPressed;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,110 +12,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
-            'Login Page',
+            'Container Widget',
             style: TextStyle(
-              color: Colors.white
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
           ),
+          centerTitle: true,
           backgroundColor: AppColors.darkBlue,
         ),
         body: Center(
-          child: Builder(
-            builder: (context) {
-              return Padding(
-                padding: const EdgeInsets.all(25),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 60.0),
-                    Image.asset('assets/images/flutter-removebg-preview.png'),
-
-                    const SizedBox(height: 10),
-
-                    TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            width: 1.0, 
-                            color: AppColors.darkGrey,
-                          ),
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            width: 1.0, 
-                            color: Colors.grey,
-                          ),
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        hintText: 'Email',
-                      ),
-                    ),
-
-                    const SizedBox(height: 10),
-
-                     TextField(
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(onPressed: () {},
-                        icon: Icon(Icons.visibility)),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            width: 1.0, 
-                            color: AppColors.darkGrey,
-                          ),
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            width: 1.0, 
-                            color: Colors.grey,
-                          ),
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        hintText: 'Password',
-                      ),
-                    ),
-                    const SizedBox(height: 15.0),
-
-                    Text('Forgot Password', style: TextStyle(color: AppColors.darkBlue),),
-
-                    const SizedBox(height: 15.0),
-
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.darkBlue,
-                        padding: EdgeInsets.symmetric(horizontal:110 , vertical: 10),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: Padding(                     
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(fontSize: 12.0,
-                          color: Colors.white),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 190.0),
-
-                    Text(
-                      'New User? Create Account',
-                      style: TextStyle(
-                        fontSize: 12.0,
-                      ),
-                    ),
-                  ],
+          child: Container(
+          height: 220,
+          width: double.infinity,
+          margin: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [
+                Colors.black,
+                Colors.blue,
+                Color.fromARGB(255, 204, 11, 238),
+                Colors.black,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: AppColors.darkBlue),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 8,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Title: Flutter Stuff',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              const SizedBox(height: 8),
+              const Divider(color: Colors.white),
+              const Text(
+                'Description: This is a card-like layout using a Container widget. It has padding, margin, and a box shadow.',
+                style: TextStyle(
+                  color: Colors.white,
                 ),
-              );
-            },
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+
+                      onPressed: () {},
+                      child: const Text(
+                        'visit to read more',
+                        style: TextStyle(
+                          
+                        ),
+                    )
+                  )
+                )
+              ],
+            ),
           ),
         ),
       ),
---
+++ styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkGrey = Color(0XFF635C5C);
+  static const darkBlue = Color(0xFF0077FF);
+}
